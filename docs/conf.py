76b8a111eae342386abe21944bd5a8b044463f1b
BungaBonga <96204800+BungaBonga@users.noreply.github.com>
2025-03-18 15:54:52 +0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,7 @@
 
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../DFDistill'))
 
@@ -18,6 +19,7 @@ project = 'DFDistill'
 copyright = '2025, Nasyrov E., Okhotnikov N., Sapronov Y., Solodkin V.'
 author = 'Nasyrov E., Okhotnikov N., Sapronov Y., Solodkin V.'
 
+master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -27,7 +29,24 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx_rtd_theme',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.inheritance_diagram',
+    'sphinx.ext.mathjax'
 ]
+
+autodoc_mock_imports = ["numpy", "scipy", "sklearn", "torch"]
+
+html_context = {
+	"display_github": True,
+	"github_user": "Intelligent-Systems-Phystech",
+	"github_repo": "DFDistill",
+	"github_version": "main",
+	"conf_py_path": "./docs"
+}
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
